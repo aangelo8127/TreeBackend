4be39467933eae05bf1eb19c51f517e0680715e0
angel.duran <angel.duran@avaldigitallabs.com>
2024-07-23 22:23:50 -0500

    test

--- src/main/java/com/mo/TreeBackendApplication.java
+++ src/main/java/com/mo/TreeBackendApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TreeBackendApplication {
 
+	public String angel;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TreeBackendApplication.class, args);
 	}
